fe0fb2974d64cbe6aa20d38a8725645e258229d0
pavanjava <manthapavankumar11@gmail.com>
2023-11-20 16:31:11 +0530

    -removed the mock UI and added the dynamic bot UI, -changed the preflight cors policies

--- src_bot/src/App.tsx
+++ src_bot/src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
+import axios, {AxiosResponse} from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type messageType = { role: string, content: string }
+
+const App = () => {
+
+    const [messages, setMessages] = useState([
+        {'role': 'bot', 'content': 'Hi! This is Jarvis, your proprietary data bot. '}
+    ]);
+
+    const [userInput, setUserInput] = useState("");
+
+    const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserInput(event.target.value)
+    }
+
+    const sendMessage = (): void => {
+        const temp: messageType[] = [];
+        messages.forEach((message: messageType) => temp.push(message))
+        temp.push({'role': 'user', 'content': userInput});
+
+        axios.post('http://localhost:8080/api/chat', {'query': userInput}).then((response: AxiosResponse<any, any>): void => {
+            temp.push({'role': 'bot', 'content': response.data.data});
+            setMessages(temp);
+            setUserInput("")
+        });
+    }
+
+    return (
+        <>
+            <div className="chat-container">
+                <div className="chat-header">PK Bot</div>
+                <div className="chat-messages" id="chat-messages" >
+                    {
+                        messages.map((object: messageType, index: number) => {
+                            if (object.role === 'bot') {
+                                return (
+                                    <div className="message bot-message" key={index}>
+                                        <div className="message-content">{'Bot: '.concat(object.content)}</div>
+                                    </div>
+
+                                )
+                            }else{
+                                return (
+                                    <div className="message user-message" key={index}>
+                                        <div className="message-content">{'User: '.concat(object.content)}</div>
+                                    </div>
+
+                                )
+                            }
+                        })
+                    }
+                </div>
+                <div className="user-input-container">
+                    <div className="input-box-container">
+                        <input type="text" className="input-box" id="user-input"
+                               placeholder="Type your message..."
+                               onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleTextChange(event)}></input>
+                        <div className="chat-icon" onClick={() => sendMessage()}>💬</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="side-container">
+                <div className="upload-button">Upload File</div>
+                <input type="file"></input>
+            </div>
+        </>
+    )
 }
 
 export default App
