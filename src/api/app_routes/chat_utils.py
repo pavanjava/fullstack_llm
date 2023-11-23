4f691ab302c319c8c69b3a02928416bf070b109f
pavanjava <manthapavankumar11@gmail.com>
2023-11-24 01:14:54 +0530

    -added text2sql utility

--- src/api/app_routes/chat_utils.py
+++ src/api/app_routes/chat_utils.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
 
 from src.core.knowledge_agent import KnowledgeAgent
+from src.core.text2sql import Text2SqlAgent
 
 knowledge_destination = "/Users/pavanmantha/Pavans/PracticeExamples/DataScience_Practice/LLMs/llama_index_tutorials/fullstack_llm/data"
 chat_routes = r = APIRouter()
@@ -14,7 +15,12 @@ class Payload(BaseModel):
 @r.post('/chat')
 async def chat(request: Payload):
     try:
-        agent = KnowledgeAgent(path=knowledge_destination)
-        return {'data': agent.query(user_query=request.query)}
+        knowledge_agent = KnowledgeAgent(path=knowledge_destination)
+        sql_agent = Text2SqlAgent()
+
+        if 'database' in request.query:
+            return {'data': sql_agent.query(prompt=request.query)}
+        else:
+            return {'data': knowledge_agent.query(user_query=request.query)}
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
